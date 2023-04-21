650b71c302d486bd568a8ac2e92dcf5653c0922d
vilas chavhan <chavhanvilas02@gmail.com>
2023-04-21 11:05:07 +0530

    implemented redux persist to store cart data in local storage

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ import App from './App';
 //using provider we provide store to our App component which can be used in any component.
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let persister = persistStore(store);
 root.render(
     <Provider store={store} >
-    <App />
+        <PersistGate persistor={persister}>
+        <App />
+        </PersistGate>
+
     </Provider>
 );
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore,combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from "redux-persist";
 import dataReducer from "../Features/dataSlice"
 import cartReducer from '../Features/cartSlice'
 //creating store, the global object where our data is stored.
 
+//this will give us access to a function that combine our reducers into an object of key/value pair.
+const reducer = combineReducers({
+    data : dataReducer,
+    cart : cartReducer,
+});
+
+//config file for the persist reducer where storage is localStorage.
+const persistConfig = {
+    key : 'cart',
+    storage,
+}
+
+//this will give us the persisted reducer.
+const persistedReducer = persistReducer(persistConfig,reducer);
+
 
 const store = configureStore({
-    reducer : {
-        data : dataReducer,
-        cart : cartReducer,
-    }
+    reducer : persistedReducer,
+    middleware : (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck : false,
+    }),
 });
 
 export default store;
