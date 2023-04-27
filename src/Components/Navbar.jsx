ecd1f52146ac9a52706f3f9b94e5096978cd6922
vilas chavhan <chavhanvilas02@gmail.com>
2023-04-27 17:10:38 +0530

    added dropdown on navbar page so that user can  see their profile with logout button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,16 @@
 import { BsFillCartCheckFill } from 'react-icons/bs'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import Profile from '../Assets/Images/Profile.png';
+import { useState } from 'react';
 export default function Navbar() {
+    const [dropdown ,setDropwdown] = useState(false);
+    const open = () => {
+        setDropwdown(!dropdown);
+    }
     //using cart.length we can check whether the cart has any products or
-    const { cart } = useSelector((state) => state);
+    const { cart, user } = useSelector((state) => state);
     return (
         <div className="bg-blue-50 flex flex-row items-center justify-between">
             <div className="text-indigo-500 text-3xl px-5 py-2 font-bold">
@@ -12,13 +19,37 @@ export default function Navbar() {
                 </Link>
             </div>
 
-            <Link to='cart'>
-                <div className='px-5 text-3xl flex items-center text-slate-800 bg-indigo-300 rounded-xl mr-5'>
+            {
+                user?.isLoggedIn === true ? (
+                    <div className='flex flex-row items-center float-right relative'>
+                        <Link to='cart'>
+                            <div className='px-5 text-3xl flex items-center text-slate-800 bg-indigo-300 rounded-xl mr-5'>
 
-                    <BsFillCartCheckFill /><span className='ml-2'>{cart.length}</span>
+                                <BsFillCartCheckFill /><span className='ml-2'>{cart.length}</span>
 
-                </div>
-            </Link>
+                            </div>
+                        </Link>
+                        <div className='flex flex-col items-center justify-center hover:cursor-pointer'>
+                            <img src={Profile} alt='profile' className='w-[8vw] sm:w-[3vw] mr-2' onClick={open} />
+                            {
+                                dropdown === true ? (
+                                    <div className='bg-blue-50 z-10 px-5 py-1 absolute top-12 right-1 text-indigo-500'>
+                                         <h6 className='mr-3 font-semibold '>{user.user.UserName}</h6>
+                                        <hr />
+                                        <button className='mt-2 hover:text-red-500'>Logout</button>
+                                    </div>
+                                ) : ''
+                            }
+                           
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <h1>Login</h1>
+                    </div>
+                )
+            }
+
         </div>
     );
 }
